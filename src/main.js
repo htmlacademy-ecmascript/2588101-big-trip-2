ccf72374ef74695c67c0534b732fdc89d63f69d2
alisameta <alisameta09@gmail.com>
2025-02-14 18:17:51 +0300

    3. Заведет презентер и отрисует компоненты на странице

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import FilterView from './view/filter-view.js';
 import HeaderInfoView from './view/header-info-view.js';
 import {render} from './render.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
 const headerContainer = document.querySelector('.trip-main');
 const filterContainer = headerContainer.querySelector('.trip-controls__filters');
+const tripEventsContainer = document.querySelector('.trip-events');
+const tripPresenter = new TripPresenter({container: tripEventsContainer});
 
 render (new FilterView(), filterContainer);
 render (new HeaderInfoView(), headerContainer, 'afterbegin');
+
+tripPresenter.init();
