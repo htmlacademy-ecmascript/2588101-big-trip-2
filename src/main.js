8c53cfb40284c60fca262ab33df142e828720c64
alisameta <alisameta09@gmail.com>
2025-02-24 13:27:36 +0300

    2.4 Подключит модель

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import FilterView from './view/filter-view.js';
 import HeaderInfoView from './view/header-info-view.js';
 import {render} from './render.js';
 import TripPresenter from './presenter/trip-presenter.js';
+import PointsModel from './model/points-model.js';
 
 const headerContainer = document.querySelector('.trip-main');
 const filterContainer = headerContainer.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
-const tripPresenter = new TripPresenter({container: tripEventsContainer});
+
+const pointsModel = new PointsModel();
+const tripPresenter = new TripPresenter({
+  container: tripEventsContainer,
+  pointsModel,
+});
 
 render (new FilterView(), filterContainer);
 render (new HeaderInfoView(), headerContainer, 'afterbegin');
