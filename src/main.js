a035c8923d5ce29240d1896cc5c10e095b583f1b
alisameta <alisameta09@gmail.com>
2025-03-27 13:15:46 +0300

    7.7 Добавит модель для фильтра. WIP

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import HeaderInfoView from './view/header-info-view.js';
 import {render} from './framework/render.js';
 import TripPresenter from './presenter/trip-presenter.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 import {generateFilter} from './mock/filter.js';
 
 const headerContainer = document.querySelector('.trip-main');
@@ -10,6 +11,7 @@ const filterContainer = headerContainer.querySelector('.trip-controls__filters')
 const tripEventsContainer = document.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
 const tripPresenter = new TripPresenter({
   container: tripEventsContainer,
   pointsModel,
