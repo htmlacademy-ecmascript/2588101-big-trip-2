3ed256da233ac19c3542d7e0c14e4089bc46683f
alisameta <alisameta09@gmail.com>
2025-04-02 13:20:01 +0300

    8.1.3 Реализует паттерн "Адаптер". WIP

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -16,7 +16,7 @@ export default class PointsModel extends Observable {
     this.#pointsApiService = pointsApiService;
 
     this.#pointsApiService.points.then((points) => {
-      console.log(points);
+      console.log(points.map(this.#adaptToClient));
     });
     this.#pointsApiService.offers.then((offers) => {
       console.log(offers);
@@ -76,5 +76,21 @@ export default class PointsModel extends Observable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient(point) {
+    const adaptedRoutePoint = {...point,
+      'basePrice': point['base_price'],
+      'dateFrom': point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      'dateTo': point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      'isFavorite': point['is_favorite'],
+    };
+
+    delete adaptedRoutePoint['base_price'];
+    delete adaptedRoutePoint['date_from'];
+    delete adaptedRoutePoint['date_to'];
+    delete adaptedRoutePoint['is_favorite'];
+
+    return adaptedRoutePoint;
   }
 }
--- src/points-api-service.js
+++ src/points-api-service.js
@@ -21,12 +21,28 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(point) {
+    const adaptedRoutePoint = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'is_favorite': point.isFavorite,
+    };
+
+    delete adaptedRoutePoint.basePrice;
+    delete adaptedRoutePoint.dateFrom;
+    delete adaptedRoutePoint.dateTo;
+    delete adaptedRoutePoint.isFavorite;
+
+    return adaptedRoutePoint;
   }
 }
